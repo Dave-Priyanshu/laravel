d68dbd0d63cb336f96cb207ae64e94b35840ee02
Dave-Priyanshu <ims30priyanshu@gmail.com>
2024-08-06 19:15:37 +0530

    Added Data Table

--- resources/views/allusers.blade.php
+++ resources/views/allusers.blade.php
@@ -9,47 +9,59 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>All Users</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css" rel="stylesheet">
 </head>
 <body>
     <div class="container">
         <div class="row">
-            <div class="col-6">
+            <div class="col-12 mb-5">
                 <h2>All Users List</h2>
-                <table class="table table-bordered table-striped">
-                    <tr>
-                        <th>ID</th>
-                        <th>Name</th>
-                        <th>Email</th>
-                        <th>Age</th>
-                        <th>City</th>
-                        <th>More Details</th>
-                        <th>Delete</th>
-                    </tr>
-                    @foreach ($data as $id => $user)
+                <table id="usersTable" class="table table-bordered table-striped ">
+                    <thead>
                         <tr>
-                            <td>{{ $user->id}}</td>
-                            <td>{{ $user->name}}</td>
-                            <td>{{ $user->email}}</td>
-                            <td>{{ $user->age}}</td>
-                            <td>{{ $user->city}}</td>
-                            <td><a href="{{ route('view.singleUser',$user->id)}}" id="viewButton" class="btn btn-primary btn-sm">View</a></td>
-                            <td><a href="{{ route('delete.user',$user->id)}}" id="viewButton" class="btn btn-danger btn-sm">Delete</a></td>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Age</th>
+                            <th>City</th>
+                            <th>More Details</th>
+                            <th>Delete</th>
                         </tr>
-                    @endforeach
+                    </thead>
+                    <tbody>
+                        @foreach ($data as $id => $user)
+                            <tr>
+                                <td>{{ $user->id }}</td>
+                                <td>{{ $user->name }}</td>
+                                <td>{{ $user->email }}</td>
+                                <td>{{ $user->age }}</td>
+                                <td>{{ $user->city }}</td>
+                                <td><a href="{{ route('view.singleUser', $user->id) }}" class="btn btn-primary btn-sm">View</a></td>
+                                <td><a href="{{ route('delete.user', $user->id) }}" class="btn btn-danger btn-sm">Delete</a></td>
+                            </tr>
+                        @endforeach
+                    </tbody>
                 </table>
             </div>
         </div>
-    </div>    
-</body>
-</html>     
+    </div>
 
-    
-    
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('#usersTable').DataTable({
+                "paging": true,
+                "searching": true,
+                "ordering": true,
+                "info": true
+            });
+        });
+    </script>
+</body>
+</html>
 @endsection
 
 @section('title')
 
 @endsection
-{{-- <h1>Post Page</h1>
-
-<a href="/about">About Page</a> --}}
--- resources/views/singleRecordUser.blade.php
+++ resources/views/singleRecordUser.blade.php
@@ -9,36 +9,66 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Single User Detail</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <style>
+        .table-wrapper {
+            margin: 20px 0;
+        }
+        .table th {
+            background-color: #f8f9fa;
+            color: #212529;
+            text-align: center;
+        }
+        .table td {
+            vertical-align: middle;
+        }
+        .btn-custom {
+            color: #fff;
+            background-color: #007bff;
+            border-color: #007bff;
+        }
+        .btn-custom:hover {
+            color: #fff;
+            background-color: #0056b3;
+            border-color: #004085;
+        }
+    </style>
 </head>
 <body>
     <div class="container">
         <div class="row">
-            <div class="col-6">
-                <h2>All Users List</h2>
-                <table class="table table-bordered table-striped">
-                    <tr>
-                        {{-- <th>ID</th> --}}
-                        <th>Name</th>
-                        <th>Email</th>
-                        <th>Age</th>
-                        <th>City</th>
-                        <th>All User Data List</th>
-                    </tr>
-                    @foreach ($data as $id => $user)
-                        <tr>
-                            {{-- <td>{{ $user->id}}</td> --}}
-                            <td>{{ $user->name}}</td>
-                            <td>{{ $user->email}}</td>
-                            <td>{{ $user->age}}</td>
-                            <td>{{ $user->city}}</td>
-                            <td><a href="/allusers" class="btn btn-primary btn-sm">View</a></td>
-                        </tr>
-                    @endforeach
-                </table>
+            <div class="col-12">
+                <h2 class="my-4">All Users List</h2>
+                <div class="table-wrapper">
+                    <table class="table table-bordered table-striped table-hover">
+                        <thead>
+                            <tr>
+                                {{-- <th>ID</th> --}}
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Age</th>
+                                <th>City</th>
+                                <th>All User Data List</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($data as $id => $user)
+                                <tr>
+                                    {{-- <td>{{ $user->id }}</td> --}}
+                                    <td>{{ $user->name }}</td>
+                                    <td>{{ $user->email }}</td>
+                                    <td>{{ $user->age }}</td>
+                                    <td>{{ $user->city }}</td>
+                                    <td>
+                                        <a href="/allusers" class="btn btn-custom btn-sm">View</a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
-    </div>    
+    </div>
 </body>
-</html>    
-
+</html>
 @endsection
